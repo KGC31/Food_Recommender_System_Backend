e240762126252434e614b331bb77aef8ddc16d3d
Nguyen Viet Kim <kimnguyenblc@gmail.com>
2026-05-03 17:03:16 +0700

    Update runserver command

--- commands/runserver.py
+++ commands/runserver.py
@@ -1,8 +1,9 @@
 import uvicorn
+import os
 
 def run(args):
-    host = "127.0.0.1"
-    port = 8000
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 8000))
     reload = False
 
     for arg in args:
@@ -20,5 +21,5 @@ def run(args):
         "app.main:app",
         host=host,
         port=port,
-        reload=reload,
+        reload=reload
     )
